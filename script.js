aa7906d08da0d9f40d0cb55687e9b3c9672cf49c
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-07 13:02:32 -0500

    more exercises

--- script.js
+++ script.js
@@ -11,11 +11,32 @@ const second = document.querySelector('li.second');
 const third = document.querySelector('ol li.third');
 
 //5. Give the section with an id of container the text “Hello!”.
-const container3 = document.querySelector('#container');
-container3.innerText = "Hello!";
+//const container3 = document.querySelector('#container');
+//container3.innerText = "Hello!";
 
 //6. Add the class main to the div with a class of footer.
 const footer = document.querySelector('div.footer');
 footer.classList.add('main');
 
+//7. Remove the class main on the div with a class of footer.
+const footer2 = document.querySelector('div.footer');
+footer2.classList.remove('main');
 
+//8. Create a new li element.
+//const newLi = document.createElement('li');
+
+//9. Give the li the text “four”.
+//newLi.innerText = "four";
+
+//10. Append the li to the ul element.
+const ul = document.querySelector('ul');
+const newLi = document.createElement('li');
+newLi.innerText = "four";
+ul.append(newLi);
+
+//11. Loop over all of the lis inside the ol tag and give them a background color of “green”.
+const list = document.querySelector('ol');
+const listItems = list.children;
+for (let listItem of listItems) {
+    listItem.style.backgroundColor = "green";
+    }
