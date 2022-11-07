cacdb631b2357cede4def6f75ac39f8860967cbd
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-07 12:06:02 -0500

    adding more

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ const container = document.getElementById('container');
 const container2 = document.querySelector('#container');
 
 //3. Select all of the list items with a class of “second”.
-const second = document.querySelectorAll('li.second');
+const second = document.querySelector('li.second');
 
 //4. Select a list item with a class of third, but only the list item inside of the ol tag.
-const third = document.querySelectorAll('ol.third');
+const third = document.querySelector('ol li.third');
 
 //5. Give the section with an id of container the text “Hello!”.
 const container3 = document.querySelector('#container');
 container3.innerText = "Hello!";
 
 //6. Add the class main to the div with a class of footer.
-
+const footer = document.querySelector('div.footer');
+footer.classList.add('main');
 
 
