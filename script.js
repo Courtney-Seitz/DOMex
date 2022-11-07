4c1c48602c34ec2f99918183a2e7eaeb16b84e05
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-07 11:51:10 -0500

    adding exs

---
+++ script.js
@@ -0,0 +1,20 @@
+//1. Select the section with an id of container without using querySelector.
+const container = document.getElementById('container');
+
+//2. Select the section with an id of container using querySelector.
+const container2 = document.querySelector('#container');
+
+//3. Select all of the list items with a class of “second”.
+const second = document.querySelectorAll('li.second');
+
+//4. Select a list item with a class of third, but only the list item inside of the ol tag.
+const third = document.querySelectorAll('ol.third');
+
+//5. Give the section with an id of container the text “Hello!”.
+const container3 = document.querySelector('#container');
+container3.innerText = "Hello!";
+
+//6. Add the class main to the div with a class of footer.
+
+
+
